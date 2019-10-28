c877a57085d239e500d2bc26005ed2d6c289e12c
Student <example@example.com>
2019-10-28 11:24:12 -0700

    refractor js

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,11 @@
 $(document).ready(function() {
   $("#form1").submit(function(event) {
-  var noun1Input = $("input#noun1").val();
-  var verbInput = $("input#verb").val();
-  var nameInput = $("input#name").val();
-  var objectInput = $("input#object").val();
-  var noun2Input = $("input#noun2").val();
-  var adjectiveInput = $("input#adjective").val();
-  var exclamationInput = $("input#exclamation").val();
-  var colorInput = $("input#color").val();
+    var blanks = ["noun1", "verb", "name", "object", "noun2", "adjective", "exclamation", "color"];
+blanks.forEach(function(blank) {
+  var userInput = $("input#" + blank).val();
+  $("." + blank).text(userInput);
 
-  $(".noun1").text(noun1Input);
-  $(".verb").text(verbInput);
-  $(".name").text(nameInput);
-  $(".object").text(objectInput);
-  $(".noun2").text(noun2Input);
-  $(".adjective").text(adjectiveInput);
-  $(".exclamation").text(exclamationInput);
-  $(".color").text(colorInput);
+});
 
   $("#story").show();
 
@@ -24,3 +13,21 @@ $(document).ready(function() {
 
 });
 });
+
+
+// $(".noun1").text(noun1Input);
+// $(".verb").text(verbInput);
+// $(".name").text(nameInput);
+// $(".object").text(objectInput);
+// $(".noun2").text(noun2Input);
+// $(".adjective").text(adjectiveInput);
+// $(".exclamation").text(exclamationInput);
+// $(".color").text(colorInput);
+// var noun1Input = $("input#noun1").val();
+// var verbInput = $("input#verb").val();
+// var nameInput = $("input#name").val();
+// var objectInput = $("input#object").val();
+// var noun2Input = $("input#noun2").val();
+// var adjectiveInput = $("input#adjective").val();
+// var exclamationInput = $("input#exclamation").val();
+// var colorInput = $("input#color").val();
